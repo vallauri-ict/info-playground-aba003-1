2a5aa411cba8b8f161b1cae0968cc0e667afac17
aba003 <59926534+aba003@users.noreply.github.com>
2021-09-19 16:46:04 +0200

    Aggiunto esercizio 01 SQL

---
+++ Esercio 01 - Prova database/Esercio 01 - Prova database/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Esercio_01___Prova_database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
