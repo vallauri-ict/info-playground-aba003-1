c76a58ec1e56f4f79747f7af9ba9ee1a7ef04eb8
aba003 <59926534+aba003@users.noreply.github.com>
2021-09-24 18:12:40 +0200

    Added Esercizio 03 - Film

---
+++ Esercizio 03  - Film/Esercizio 03  - Film/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Esercizio_03____Film
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
