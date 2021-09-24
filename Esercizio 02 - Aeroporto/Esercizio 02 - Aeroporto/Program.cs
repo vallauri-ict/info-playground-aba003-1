05b2bafbdc675bfd95d23d1923a361722db7bcd8
aba003 <59926534+aba003@users.noreply.github.com>
2021-09-24 09:09:43 +0200

    Added Esercizio 02 - Aeroporto

---
+++ Esercizio 02 - Aeroporto/Esercizio 02 - Aeroporto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Esercizio_02___Aeroporto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
