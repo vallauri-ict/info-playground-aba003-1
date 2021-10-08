8f6ca2d855f5b4c3306da1e3e1cbfdb18d3193ae
Ababei Edoardo <e.ababei.1257@vallauri.edu>
2021-10-08 11:20:05 +0200

    Added queries es 05

--- Esercizio 05 - Automobili/SQLQuery1.sql
+++ Esercizio 05 - Automobili/SQLQuery1.sql
@@ -1,42 +1,23 @@
-﻿CREATE TABLE Automobile(
-	Targa varchar(30) not null,
-	Marca varchar(30) not null,
-	Cilindrata int not null,
-	Potenza int not null,
-	CodF int not null,
-	CodAss int not null,
-	primary key(Targa),
-	CONSTRAINT FK_CodF FOREIGN KEY (CodF) REFERENCES Proprietari(CodF),
-	CONSTRAINT FK_CodAss foreign key (CodAss) references Assicurazioni(CodAss)
-);
+﻿/*Targa e Marca delle Auto di cilindrata superiore a 2000 cc o di potenza superiore a 120 CV*/
+select a.Targa as Targa , a.Marca as Marca from Automobile a where a.Cilindrata>2000 and a.Potenza>120
 
-create table Proprietari(
-	CodF int not null,
-	Nome varchar(30)  not null,
-	Residenza varchar(30) not null,
-	primary key (CodF)
-)
+/*Nome del proprietario e Targa delle Auto di cilindrata superiore a 2000 cc oppure di potenza superiore a 120 CV*/
+select p.Nome as Nome ,a.Targa  from Automobile a, Proprietari p where a.Cilindrata>2000 and a.Potenza>120 and p.CodF=a.CodF;
 
-create table Assicurazioni(
-	CodAss int not null,
-	Nome varchar(30) not null,
-	Sede varchar(30) not null,
-	primary key(CodAss)
-)
+/*Targa e Nome del proprietario delle Auto di cilindrata superiore a 2000 cc oppure di potenza
+superiore a 120 CV, assicurate presso la “San paolo”*/
 
-create table Sinistro (
-	CodS int not null,
-	Località varchar(30) not null,
-	DataSinistro date  not null,
-	primary key (CodS)
-)
+select p.Nome as Nome ,a.Targa  from Automobile a, Proprietari p,Assicurazioni ass where a.Cilindrata>2000 and a.Potenza>120 and p.CodF=a.CodF and a.CodAss=ass.CodAss and ass.Nome='Axa' 
 
-create table AutoCoinvolte(
-  CodS int not null,
-  Targa varchar(30) not null, 
-   ImportoDelDanno int not null,
-  primary key(CodS),
-  CONSTRAINT FK_CodS foreign key (CodS) references Sinistro(CodS),
-  CONSTRAINT FK_Targa foreign key (Targa) references Automobile(Targa)
-)
+/*Targa e Nome del proprietario delle Auto assicurate presso la “San Paolo” e coinvolte in sinistri il 28/02/2020*/
+select a.Targa,p.Nome from Sinistro s, Proprietari p, Automobile a, AutoCoinvolte autCoinv, Assicurazioni ass where s.CodS=autCoinv.CodS and a.CodF=p.CodF and a.CodAss =ass.CodAss 
+and a.Targa= autCoinv.Targa and ass.Nome='San Paolo' and s.DataSinistro='2020-02-28'
 
+/*Per ciascuna Assicurazione, il nome, la sede ed il numero di auto assicurate*/
+select ass.Nome,ass.Sede, COUNT(*) from Assicurazioni ass, Automobile a where a.CodAss=ass.CodAss group by ass.Nome , ass.Sede;
+
+/*Per ciascuna auto “Fiat”, la targa dell’auto ed il numero di sinistri in cui è stata coinvolta*/
+select a.Targa, COUNT(*) from Automobile a,Sinistro s , AutoCoinvolte autoCoinvolte where a.Targa=autoCoinvolte.Targa and autoCoinvolte.CodS=s.CodS and a.Marca='Fiat' group by a.Targa
+
+/*Per ciascuna auto coinvolta in più di un sinistro, la targa dell’auto, il nome dell’ Assicurazione
+ed il totale dei danni riportati*/
